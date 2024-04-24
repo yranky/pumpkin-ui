fa3d046cd4cca1123e10ad6bc6fd3ab0a4d94b2e
yranky <douye@douye.top>
2024-04-24 21:57:16 +0800

    ✨ feat: 完善picker-view组件

--- packages/mobile/src/picker-view/src/picker-view.vue
+++ packages/mobile/src/picker-view/src/picker-view.vue
@@ -7,13 +7,28 @@
         ]">
             <div :class="[
                 bem.e('column')
-            ]">
+            ]" :style="{
+                height: visibleItemCount * itemHeight + 'px'
+            }">
                 <div :class="[
-                    bem.e('column-item')
-                ]" v-for="item in 10" :key="item">1212</div>
+                    bem.e('item')
+                ]" :style="{
+                    height: itemHeight + 'px'
+                }" v-for="item in 10" :key="item">1212</div>
             </div>
         </div>
+        <div :class="[
+            bem.e('mask'),
+            bem.m('top')
+        ]">
 
+        </div>
+        <div :class="[
+            bem.e('mask'),
+            bem.m('bottom')
+        ]">
+
+        </div>
     </div>
 </template>
 <script lang="ts" setup>
