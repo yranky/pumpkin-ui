46dbe59f534404c45e675aba78ea3eaa2e9355e4
yranky <douye@douye.top>
2024-06-30 17:33:39 +0800

    📃 docs: 文档修改

--- docs/mobile/.vitepress/config.ts
+++ docs/mobile/.vitepress/config.ts
@@ -1,8 +1,27 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
-    lang: 'zh-CN',
-    title: 'PK-UI',
-    description: 'PK-UI 组件库 - 移动端',
-    base: '/mobile'
+    lastUpdated: true,
+    cleanUrls: true,
+    metaChunk: true,
+    rewrites: {
+        'zh-CN/:rest*': ':rest*'
+    },
+    themeConfig: {
+        i18nRouting: true,
+        search: {
+            provider: 'local'
+        }
+    },
+    locales: {
+        root: {
+            title: 'PK-UI',
+            description: 'PK-UI 组件库 - 移动端',
+            label: '简体中文',
+            lang: 'zh-CN',
+            themeConfig: {
+
+            }
+        }
+    }
 })
