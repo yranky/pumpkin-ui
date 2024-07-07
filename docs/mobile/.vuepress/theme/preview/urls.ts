f09a5b100ed632d1e61a0687634098033dbc38e3
yranky <douye@douye.top>
2024-07-07 20:54:42 +0800

    🐳 chore: 修改脚本

--- docs/mobile/.vuepress/theme/preview/urls.ts
+++ docs/mobile/.vuepress/theme/preview/urls.ts
@@ -1,13 +1,13 @@
 import { withBase } from "vuepress/client"
 
-const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:5173' : withBase('/demo/')
+const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:5173' : withBase('/demo/index.html')
 
 export default [
     {
         activeMatch: '/components/button',
-        url: BASE_URL + '/#/button'
+        url: BASE_URL + '#/button'
     }, {
         activeMatch: '/components/cell',
-        url: BASE_URL + '/#/cell'
+        url: BASE_URL + '#/cell'
     }
 ];
