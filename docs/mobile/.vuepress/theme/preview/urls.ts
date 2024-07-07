f98e6dd3c946cc27b96b29fc9cefa4f13c009a70
yranky <douye@douye.top>
2024-07-07 20:53:24 +0800

    🐳 chore: 文档配置修改

--- docs/mobile/.vuepress/theme/preview/urls.ts
+++ docs/mobile/.vuepress/theme/preview/urls.ts
@@ -1,6 +1,6 @@
 import { withBase } from "vuepress/client"
 
-const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:5173' : withBase('/demo/index.html')
+const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:5173' : withBase('/demo/')
 
 export default [
     {
