7d6fe45294e0b19adfb6fcbb16a6428d8a25e65b
yranky <douye@douye.top>
2024-10-17 20:47:14 +0800

    📃 docs: 完善Search demo

--- demo/mobile/src/views/search/Search.vue
+++ demo/mobile/src/views/search/Search.vue
@@ -1,18 +1,42 @@
 <template>
     <div class="search-container">
-        <pk-search placeholder="请输入关键词">
+        <pk-divider>基础用法</pk-divider>
+        <pk-search />
+
+        <pk-divider>事件监听</pk-divider>
+        <pk-search @on-search="onSearch" />
+
+        <pk-divider>隐藏搜索按钮</pk-divider>
+        <pk-search :show-search-button="false" />
+
+        <pk-divider>禁用</pk-divider>
+        <pk-search disabled :show-search-button="false" />
+
+        <pk-divider>输入框对齐方式</pk-divider>
+        <pk-search input-text-align="center" />
+
+        <pk-divider>插槽</pk-divider>
+        <pk-search>
+            <template #left-icon>icon</template>
             <template #left>left</template>
+            <template #right-icon>icon</template>
             <template #right>right</template>
         </pk-search>
+
     </div>
 </template>
 <script lang="ts" setup>
-import { PkSearch } from '@pumpkin-ui/mobile'
+import { PkSearch, PkDivider, Toast } from '@pumpkin-ui/mobile'
 import { ref } from 'vue'
+
+const onSearch = (value) => {
+    Toast.show(`搜索内容：${value}`);
+}
 
 </script>
 <style lang="less" scoped>
 .search-container {
-    padding: 10px 0;
+    min-height: 100vh;
+    background-color: #f5f5f5;
 }
 </style>
