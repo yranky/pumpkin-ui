4ec16dba8ae7f10b5312cb07ba9312bbb068b20d
yranky <douye@douye.top>
2025-08-27 14:38:21 +0800

    📃 docs: 完善Tab文档

--- demo/mobile/src/views/tab/Tab.vue
+++ demo/mobile/src/views/tab/Tab.vue
@@ -1,75 +1,54 @@
 <template>
     <div class="tab-container">
-        <pk-tabs v-model="currentModel" @on-change="onChange">
-            <pk-tab :title="tabText" v-if="showTab1" :name="1"></pk-tab>
-            <pk-tab title="标签2" :name="2">2</pk-tab>
-            <pk-tab title="标签3" :name="3">3</pk-tab>
-            <pk-tab title="标签4" :name="4">4</pk-tab>
-            <pk-tab title="标签5" :name="5">5</pk-tab>
-            <pk-tab title="标签6" :name="6">6</pk-tab>
-            <pk-tab title="标签7" :name="7">7</pk-tab>
-            <pk-tab title="标签8" :name="8">8</pk-tab>
-            <pk-tab title="标签9" :name="9">9</pk-tab>
-            <pk-tab title="标签0" :name="99">0</pk-tab>
-            <!-- <pk-tab title="标签1" v-if="showTab1"></pk-tab>
-            <pk-tab title="标签66" v-if="showTab1"></pk-tab>
-            <pk-tab title="标签666" v-if="showTab1"></pk-tab>
-            <pk-tab title="标签66666" v-if="showTab2"></pk-tab> -->
-        </pk-tabs>
-        <pk-tabs v-model="currentModel" @on-change="onChange" :scrollable="false">
-            <pk-tab :title="tabText" v-if="showTab1" :name="1"></pk-tab>
-            <pk-tab title="标签2" :name="2">2</pk-tab>
-            <pk-tab title="标签3" :name="3">3</pk-tab>
-            <pk-tab title="标签4" :name="4">4</pk-tab>
-            <pk-tab title="标签5" :name="5">5</pk-tab>
-            <pk-tab title="标签6" :name="6">6</pk-tab>
-            <pk-tab title="标签7" :name="7">7</pk-tab>
-            <pk-tab title="标签8" :name="8">8</pk-tab>
-            <pk-tab title="标签9" :name="9">9</pk-tab>
-            <pk-tab title="标签0" :name="99">0</pk-tab>
-            <!-- <pk-tab title="标签1" v-if="showTab1"></pk-tab>
-            <pk-tab title="标签66" v-if="showTab1"></pk-tab>
-            <pk-tab title="标签666" v-if="showTab1"></pk-tab>
-            <pk-tab title="标签66666" v-if="showTab2"></pk-tab> -->
-        </pk-tabs>
+        <pk-divider>基本使用</pk-divider>
         <pk-tabs>
-            <pk-tab title="标签2">标签2</pk-tab>
-            <pk-tab title="标签3">标签3s</pk-tab>
+            <pk-tab title="标签1">标签1内容</pk-tab>
+            <pk-tab title="标签2">标签2内容</pk-tab>
+        </pk-tabs>
+        <pk-divider>可滑动</pk-divider>
+        <pk-tabs>
+            <pk-tab title="标签1" :name="1">1</pk-tab>
+            <pk-tab title="标签2" :name="2">2</pk-tab>
+            <pk-tab title="标签3" :name="3">3</pk-tab>
+            <pk-tab title="标签4" :name="4">4</pk-tab>
+            <pk-tab title="标签5" :name="5">5</pk-tab>
+            <pk-tab title="标签6" :name="6">6</pk-tab>
+            <pk-tab title="标签7" :name="7">7</pk-tab>
+            <pk-tab title="标签8" :name="8">8</pk-tab>
+            <pk-tab title="标签最后一个" :name="9">9</pk-tab>
+        </pk-tabs>
+        <pk-divider>默认值</pk-divider>
+        <pk-tabs :model-value="5">
+            <pk-tab title="标签1" :name="1">1</pk-tab>
+            <pk-tab title="标签2" :name="2">2</pk-tab>
+            <pk-tab title="标签3" :name="3">3</pk-tab>
+            <pk-tab title="标签4" :name="4">4</pk-tab>
+            <pk-tab title="标签5" :name="5">5</pk-tab>
+            <pk-tab title="标签6" :name="6">6</pk-tab>
+            <pk-tab title="标签7" :name="7">7</pk-tab>
+            <pk-tab title="标签8" :name="8">8</pk-tab>
+            <pk-tab title="标签最后一个" :name="9">9</pk-tab>
+        </pk-tabs>
+
+
+
+        <pk-divider>onChange</pk-divider>
+        <pk-tabs @on-change="(value, title) => Toast.show(`title:${title},value:${value}`)">
+            <pk-tab title="标签1" :name="1">1</pk-tab>
+            <pk-tab title="标签2" :name="2">2</pk-tab>
+            <pk-tab title="标签3" :name="3">3</pk-tab>
+            <pk-tab title="标签4" :name="4">4</pk-tab>
+            <pk-tab title="标签5" :name="5">5</pk-tab>
+            <pk-tab title="标签6" :name="6">6</pk-tab>
+            <pk-tab title="标签7" :name="7">7</pk-tab>
+            <pk-tab title="标签8" :name="8">8</pk-tab>
+            <pk-tab title="标签最后一个" :name="9">9</pk-tab>
         </pk-tabs>
     </div>
 </template>
 <script lang="ts" setup>
-import { PkTab, PkTabs } from '@pumpkin-ui/mobile'
-import { ref, watch } from 'vue'
+import { PkTab, PkTabs, PkDivider, Toast } from '@pumpkin-ui/mobile'
 
-const showTab1 = ref(false)
-const showTab2 = ref(false)
-
-const currentModel = ref(99)
-
-watch(currentModel, (val) => {
-    console.log('currentModel', val)
-}, {
-    immediate: true
-})
-
-const tabText = ref('标签6')
-
-setTimeout(() => {
-    tabText.value = '标签6-1'
-}, 8000)
-
-setTimeout(() => {
-    showTab1.value = true
-
-    setTimeout(() => {
-        showTab2.value = true
-    })
-}, 3000)
-
-const onChange = (val, title) => {
-    console.log(val, title)
-}
 </script>
 <style lang="less" scoped>
 .tab-container {
--- docs/mobile/components/tab.md
+++ docs/mobile/components/tab.md
@@ -8,8 +8,131 @@ aside: false
 
 ## 基本使用
 ```vue
-<pk-tabs>
-    <pk-tab></pk-tab>
-    <pk-tab></pk-tab> 
-</pk-tabs>
+  <pk-tabs>
+    <pk-tab title="标签1">标签1内容</pk-tab>
+    <pk-tab title="标签2">标签2内容</pk-tab>
+   </pk-tabs>
 ```
+
+
+## 代码示例
+### 基础用法
+::: code-tabs#default
+@tab template
+
+```vue [template]
+    <pk-tabs>
+        <pk-tab title="标签1">标签1内容</pk-tab>
+        <pk-tab title="标签2">标签2内容</pk-tab>
+    </pk-tabs>
+```
+
+@tab script
+```js [script]
+import { PkTab, PkTabs } from '@pumpkin-ui/mobile'
+
+```
+
+@tab style
+```css [style]
+
+```
+:::
+
+
+
+
+### 可滑动
+默认就是可滑动的，可通过`scrollable`设置为`false`关闭可滑动。
+::: code-tabs#scrollable
+@tab template
+
+```vue [template]
+    <pk-tabs>
+        <pk-tab title="标签1" :name="1">1</pk-tab>
+        <pk-tab title="标签2" :name="2">2</pk-tab>
+        <pk-tab title="标签3" :name="3">3</pk-tab>
+        <pk-tab title="标签4" :name="4">4</pk-tab>
+        <pk-tab title="标签5" :name="5">5</pk-tab>
+        <pk-tab title="标签6" :name="6">6</pk-tab>
+        <pk-tab title="标签7" :name="7">7</pk-tab>
+        <pk-tab title="标签8" :name="8">8</pk-tab>
+        <pk-tab title="标签最后一个" :name="9">9</pk-tab>
+    </pk-tabs>
+```
+
+@tab script
+```js [script]
+import { PkTab, PkTabs } from '@pumpkin-ui/mobile'
+
+```
+
+@tab style
+```css [style]
+
+```
+:::
+
+
+
+
+
+### 默认值
+可通过`model-value`设置默认值。也可以通过`v-model`实现双向绑定。
+
+::: code-tabs#v-model
+@tab template
+
+```vue [template]
+    <pk-tabs :model-value="5">
+        <pk-tab title="标签1" :name="1">1</pk-tab>
+        <pk-tab title="标签2" :name="2">2</pk-tab>
+        <pk-tab title="标签3" :name="3">3</pk-tab>
+        <pk-tab title="标签4" :name="4">4</pk-tab>
+        <pk-tab title="标签5" :name="5">5</pk-tab>
+        <pk-tab title="标签6" :name="6">6</pk-tab>
+        <pk-tab title="标签7" :name="7">7</pk-tab>
+        <pk-tab title="标签8" :name="8">8</pk-tab>
+        <pk-tab title="标签最后一个" :name="9">9</pk-tab>
+    </pk-tabs>
+```
+
+@tab script
+```js [script]
+import { PkTab, PkTabs } from '@pumpkin-ui/mobile'
+
+```
+
+@tab style
+```css [style]
+
+```
+:::
+
+
+
+
+### 事件监听
+可以监听`on-change`事件。
+::: code-tabs#onChange
+@tab template
+
+```vue [template]
+   <pk-tabs @on-change="(value, title) => Toast.show(`title:${title},value:${value}`)">
+        <pk-tab title="标签1" :name="1">1</pk-tab>
+        <pk-tab title="标签2" :name="2">2</pk-tab>
+        <pk-tab title="标签3" :name="3">3</pk-tab>
+        <pk-tab title="标签4" :name="4">4</pk-tab>
+        <pk-tab title="标签5" :name="5">5</pk-tab>
+        <pk-tab title="标签6" :name="6">6</pk-tab>
+        <pk-tab title="标签7" :name="7">7</pk-tab>
+        <pk-tab title="标签8" :name="8">8</pk-tab>
+        <pk-tab title="标签最后一个" :name="9">9</pk-tab>
+    </pk-tabs>
+```
+
+@tab script
+```js [script]
+import { PkTab, PkTabs, Toast } from '@pumpkin-ui/mobile'
+```
+:::
