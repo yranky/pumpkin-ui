41f9a08d8e9e72c889bc5b2e7fdc3ed997784a41
yranky <douye@douye.top>
2025-08-28 20:55:57 +0800

    🐳 chore: 完善测试包配置

--- packages/mobile/jest.config.js
+++ packages/mobile/jest.config.js
@@ -1,8 +1,12 @@
 export default {
+    testEnvironment: 'jsdom',
     transform: {
         '^.+\\.ts?$': 'ts-jest',
         '\\.(less|css)$': 'jest-less-loader',
         '^.+\\.vue$': '@vue/vue3-jest'
     },
-    modulePathIgnorePatterns: ['<rootDir>/dist'],
+    moduleNameMapper: {
+        '@pumpkin-ui/mobile': '<rootDir>/dist/lib/index.js' // 或者您dist目录下的入口文件
+    },
+    modulePathIgnorePatterns: ['<rootDir>/package.json'],
 }
